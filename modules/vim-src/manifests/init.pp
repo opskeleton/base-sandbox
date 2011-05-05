2ccb7a7a60fd6bd1511ec2a29cc7cd3b2d9b6a79
narkisr <narkisr@gmail.com>
2011-05-06 01:20:54 +0300

    gui configuration working

--- modules/vim-src/manifests/init.pp
+++ modules/vim-src/manifests/init.pp
@@ -19,6 +19,11 @@ class vim-src {
    "10.10" => "python2.6"
   }
 
+  #Add the following if desktop install: libncurses-dev libgnome2-dev libgtk2.0-dev libatk1.0-dev libbonoboui2-dev libcairo2-dev libx11-dev libxpm-dev libxt-dev
+   $config_flags = $is_desktop? {
+     "true"  => "--with-features=huge --enable-gui=gnome2",
+     "false" => "--with-features=big"
+   }  
 
    package {"mercurial":
     ensure => "installed"
@@ -46,7 +51,7 @@ class vim-src {
   }
 
  exec {"configure_vim" :
-    command => "/tmp/vim/configure --enable-multibyte --enable-cscope --enable-xim --enable-rubyinterp --enable-pythoninterp --with-python-config-dir=/usr/lib/$python/config --disable-largefile --with-features=big",
+    command => "/tmp/vim/configure --enable-multibyte --enable-cscope --enable-xim --enable-rubyinterp --enable-pythoninterp --with-python-config-dir=/usr/lib/$python/config --disable-largefile $config_flags",
     cwd     => "/tmp/vim/",
     path    => ["/usr/bin/","/bin/"],
     require => [Package["ncurses-dev"], Package["python-dev"],Vcsrepo["/tmp/vim"]],
