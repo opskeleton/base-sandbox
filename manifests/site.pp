e3ac1873523303bc62dfb3cb42598ce6da7af1aa
narkisr <narkisr@gmail.com>
2011-05-27 13:49:24 +0300

    adding user to z and using indluce instead of inherit

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,20 +3,23 @@ $user = "ronen"
 $www_user="narkisr"
 $email = "narkisr@gmail.com"
 
+stage {"pre": before => Stage["main"]}
+Vcsrepo {stage => "main"}
 class basenode {
+  class { git: user=> $www_user, email => $email,stage => pre}
   include "build_essential"
   include "vim-src"
-  class { git: user=> $www_user, email => $email}
   class { vim-configuration: user => $user}
   class { "zsh": user => $user}
   include "zsh_configuration"
-  include "z"
+  class { "z": user => $user}
   include "local_security"
   include "apt"
   include "apt::unattended-upgrade::automatic"
 }
 
-class development inherits basenode {
+class development {
+  include basenode
   include "nodejs"
   include "nodejs::npm"
   include "coffeescript"
@@ -27,6 +30,10 @@ node "puppet" {
 }
 
 node "puppet-client" {
+ include development
+}
+
+node "puppet-client2" {
  include development
 }
 
