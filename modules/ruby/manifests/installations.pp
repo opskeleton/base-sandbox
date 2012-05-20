12d649b7d1a54dec88522550957b80f938b25afb
narkisr <narkisr@gmail.com>
2012-05-20 03:16:02 +0300

    fixing snipmate

--- modules/ruby/manifests/installations.pp
+++ modules/ruby/manifests/installations.pp
@@ -8,20 +8,23 @@ class ruby::installations {
   $ruby8 = 'ruby-1.8.7'
 
 
-  if $rvm_installed=="true" {
+  if $rvm_installed==true {
     rvm_system_ruby {
       $ruby9:
-        ensure => 'present',
-        default_use => true;
-      $ruby8:
-        ensure => 'present',
+        ensure      => 'present',
         default_use => false;
+      $ruby8:
+        ensure      => 'present',
+        default_use => true;
     }
 
     rvm_gem {
       'bundler':
         ruby_version => $ruby8,
-        require => Rvm_system_ruby[$ruby8];
+        require      => Rvm_system_ruby[$ruby8];
+      'rake':
+        ruby_version => $ruby8,
+        require      => Rvm_system_ruby[$ruby8];
     }
   }
 }
--- modules/vim-configuration/manifests/snipmate.pp
+++ modules/vim-configuration/manifests/snipmate.pp
@@ -1,12 +1,12 @@
 class vim-configuration::snipmate($dot_vim) {
 
   exec{'deploy snipmate-snippets':
-    command  => 'rake deploy_local',
-    cwd	   => "$dot_vim/bundle/snipmate-snippets", 
-    user	   => $username,
-    path     => ['/usr/local/rvm/bin/','/bin'],
-    require  => [Git::Clone[$dot_vim],Package['ruby1.8-dev'],Exec['.vim submodules']],
-    subscribe => Git::Clone[$dot_vim],
+    command     => 'rvm use system && rake deploy_local',
+    cwd         => "$dot_vim/bundle/snipmate-snippets",
+    user        => $username,
+    path        => ['/usr/local/rvm/bin/','/bin','/usr/bin'],
+    require     => [Git::Clone[$dot_vim],Package['ruby1.8-dev'],Exec['.vim submodules']],
+    subscribe   => Git::Clone[$dot_vim],
     refreshonly => true
   }
 
