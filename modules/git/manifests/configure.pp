dd796b41e01eea4ba8b15a01f45c1f23fd4617e0
narkisr <narkisr@gmail.com>
2012-05-31 03:09:01 +0300

    adding git configuration and templates

--- modules/git/manifests/configure.pp
+++ modules/git/manifests/configure.pp
@@ -1,25 +1,30 @@
 class git::configure($git_user="", $git_email="") {
-  	exec{"set git global user":
-  		command	=> "git config --global user.name $git_user",
-  		path        => ["/usr/bin", "/usr/sbin"],
-  		user    	=> "root",
-  		subscribe	=>  Package["git-core"], 
-  		refreshonly => true
-  	}
 
-  	exec{"set git global email":
-  		command	=> "git config --global user.email $git_email",
-  		path        => ["/usr/bin", "/usr/sbin"],
-  		user	      => "root",
-  		subscribe	=>  Package["git-core"],
-  		refreshonly => true
-  	}
+  $home = "/home/$username"
 
-  	exec{"set git global editor":
-  		command	=> "git config --global core.editor vim",
-  		path        => ["/usr/bin", "/usr/sbin"],
-  		user	      => "root",
-  		subscribe	=>  Package["git-core"],
-  		refreshonly => true
-  	}
+  define git_config( $value) {
+    exec {"set git $name $value":
+      command	=> "git config --global $name $value",
+      path        => ["/usr/bin", "/usr/sbin"],
+      user    	=> "root",
+      require  => Package['git-core']
+    }
+  }
+
+  git_config{'user.name': value => $git_user }
+  git_config{ 'user.email': value => $git_email } 
+  git_config{ 'core.editor': value => 'vim' } 
+  git_config{ 'init.templatedir': value => '~/.git_template' } 
+  git_config {'alias.sub-add': value => "'submodule add'"}
+  git_config {'alias.sub-up': value => "'submodule update --init'"}
+  git_config {'alias.cm': value => "'commit -m'"}
+  git_config {'http.sslVerify': value => 'false'}
+
+  git::clone {'git_template':
+    url   => 'git://github.com/narkisr/git_template.git',
+    dst   => "$home/.git_template",
+    owner => $username
+  }
+ 
+
 }
