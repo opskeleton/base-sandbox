189540c36d2ac17dcda34702ea766a5f6c397870
narkisr <narkisr@gmail.com>
2012-09-26 01:13:56 +0200

    using absolute path and subscribing to submodule clone

--- modules/vim-configuration/manifests/snipmate.pp
+++ modules/vim-configuration/manifests/snipmate.pp
@@ -1,12 +1,12 @@
 class vim-configuration::snipmate($dot_vim) {
 
    exec{'deploy snipmate-snippets':
-     command     => 'rsync -av --delete . ~/.vim/bundle/snipmate/snippets/',
+     command     => "rsync -av --delete . $dot_vim/bundle/snipmate/snippets/",
      cwd         => "$dot_vim/bundle/snipmate-snippets",
      user        => $username,
      path        => ['/usr/local/rvm/bin/','/bin','/usr/bin',],
      require     => [Git::Clone[$dot_vim],Exec['.vim submodules']],
-     subscribe   => Git::Clone[$dot_vim],
+     subscribe   => Exec['.vim submodules'],
      refreshonly => true
    }
 
