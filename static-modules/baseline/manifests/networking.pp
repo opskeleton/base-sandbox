0faac9ddd13140d4f3c7a1e88ea41d64eea8d6c6
narkisr <narkisr@gmail.com>
2013-01-18 22:47:35 +0200

    using hiera

--- static-modules/baseline/manifests/networking.pp
+++ static-modules/baseline/manifests/networking.pp
@@ -1,10 +1,11 @@
 # Basic neworking utilities
 class baseline::networking {
 
+  $home = hiera('home')
+
   package{['sshuttle','wakeonlan','nmap']:
     ensure  => present
   }
-
 
   file{"${home}/.ssh/config":
     source  => 'puppet:///modules/baseline/ssh_config'
