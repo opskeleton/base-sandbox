5ea5159761cd73fe342fa9dd15c24b6e1511b088
narkisr <narkisr@gmail.com>
2013-05-07 23:37:23 +0300

    adding rvm deps

--- static-modules/langs/manifests/ruby.pp
+++ static-modules/langs/manifests/ruby.pp
@@ -1,7 +1,7 @@
 # Class: ruby
 #
 # This module manages ruby
-class ruby {
+class langs::ruby {
 
   package{['rubygems','rake','ruby1.9.1-dev']:
     ensure  => installed
@@ -14,19 +14,24 @@ class ruby {
   $ruby9 = 'ruby-1.9.3'
 
   if $rvm_installed=="true" {
-    rvm_system_ruby {
-      $ruby9:
-        ensure      => 'present',
-        default_use => true;
-    }
+    package{['libgdbm-dev', 'libncurses5-dev', 'libtool',
+              'pkg-config', 'libffi-dev']:
+      ensure  => present
+      } ->
 
-    rvm_gem {
-      'bundler':
-        ruby_version => $ruby9,
-        require      => Rvm_system_ruby[$ruby9];
-      'rake':
-        ruby_version => $ruby9,
-        require      => Rvm_system_ruby[$ruby9];
-    }
+      rvm_system_ruby {
+        $ruby9:
+          ensure      => 'present',
+          default_use => true;
+      }
+
+      rvm_gem {
+        'bundler':
+          ruby_version => $ruby9,
+          require      => Rvm_system_ruby[$ruby9];
+        'rake':
+          ruby_version => $ruby9,
+          require      => Rvm_system_ruby[$ruby9];
+      }
   }
 }
