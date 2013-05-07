3764e6a493fcec4ba13ae7b5341c1daa72e4d217
narkisr <narkisr@gmail.com>
2013-05-07 23:37:35 +0300

    new md5

--- static-modules/shell/manifests/git_fat.pp
+++ static-modules/shell/manifests/git_fat.pp
@@ -6,7 +6,7 @@ class shell::git_fat {
   downloadfile::and_md5check { 'git-fat':
       url    => 'https://raw.github.com/jedbrown/git-fat/master/git-fat',
       dest   => "${$home}/bin/git-fat",
-      md5sum => 'de2c66d07e7523ded7629fc0197423bd',
+      md5sum => '653bfbb3069a7d227806919487ecb0a4',
       user   => hiera('user'), 
       group  => hiera('user'),
       chmod  => '770',
