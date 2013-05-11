3a21da56c4e35aaa4883342c983390e723479174
narkisr <narkisr@gmail.com>
2013-05-11 05:58:56 +0300

    adding node

--- manifests/default.pp
+++ manifests/default.pp
@@ -5,24 +5,17 @@ $scm_user = hiera('scm_user')
 $scm_email = hiera('scm_email')
 $home = hiera('home')
 
-class { 'apt':
-  always_apt_update    => false,
-  disable_keys         => undef,
-  proxy_host           => false,
-  proxy_port           => '8080',
-  purge_sources_list   => false,
-  purge_sources_list_d => false,
-  purge_preferences_d  => false
-}
+node 'puppet-base-env.local' {
 
-include git
-include shell
-include baseline
-include langs
-include vim
-include build
-class {'desktop':
-  home     => $home,
-  username => $username
-}
+  include git
+  include shell
+  include baseline
+  include langs
+  include vim
+  include build
+  class {'desktop':
+    home     => $home,
+    username => $username
+  }
 
+}
