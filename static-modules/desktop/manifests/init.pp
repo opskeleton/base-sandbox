1108347b97aeab5b232ff5af555e15dfab573b0c
Ronen Narkis <narkisr@gmail.com>
2013-10-27 15:13:08 +0200

    cleaning up

--- static-modules/desktop/manifests/init.pp
+++ static-modules/desktop/manifests/init.pp
@@ -1,39 +1,18 @@
-# == Class: modules/desktop
-#
-# Sets up a list of desktop packages
-# (checks if its an Ubuntu based desktop system first).
-#
-# === Examples
-#
-# include desktop
-#
-# === Authors
-#
-# Ronen Narkis narkisr@gmail.com
-#
-# === Copyright
-#
-# Copyright 2011 narkisr.com, unless otherwise noted.
-#
+# setting up a desktop installation
 class desktop($username='',$home='') {
 
   if($desktop::is_desktop == 'true'){
+
+    include virtualbox
+    include desktop::misc
+
     class{'desktop::xmonad':
       home     => hiera('home'),
       username => hiera('user')
     }
-    # include desktop::rxvt
-    # include desktop::spotify
-    # include desktop::netflix
-    include desktop::misc
 
-    class { 'virtualbox': }
     package{'gparted':
       ensure  => present
     }
-
-  #  class { 'idea::community':
-  #    version => '12.0.2',
-  #  }
   }
 }
