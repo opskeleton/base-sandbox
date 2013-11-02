17ca9f5184671507d1a63d4f7368d7a09d9a6d82
Ronen Narkis <narkisr@gmail.com>
2013-11-02 23:41:49 +0200

    clearing langs

--- static-modules/langs/manifests/build_essential.pp
+++
@@ -1,8 +0,0 @@
-class build_essential {
-
-  if (!defined(Package['build-essential'])) {
-    package{"build-essential":
-      ensure	=> "installed"
-    }
-  }
-}
--- static-modules/langs/manifests/init.pp
+++
@@ -1,8 +0,0 @@
-# A class which sets up programming languages
-class langs {
-  include jdk
-  include ruby
-  include langs::erlang
-  include coffeescript
-  include build_essential
-}
