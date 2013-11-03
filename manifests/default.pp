a5b6d104639726a582777d8aabe85460601ca15b
Ronen Narkis <narkisr@gmail.com>
2013-11-03 02:39:33 +0200

    changing to default

--- manifests/default.pp
+++ manifests/default.pp
@@ -5,9 +5,8 @@ $scm_user = hiera('scm_user')
 $scm_email = hiera('scm_email')
 $home = hiera('home')
 
-node 'puppet-base-env.local' {
+node default {
 
-  import 'backups.pp'
   include git
   include shell
   include baseline
