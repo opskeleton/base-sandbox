4a69b6022bfbe95373f3875f8a7e024561e11a19
narkisr <narkisr@gmail.com>
2014-05-25 21:42:10 +0300

    adding sysctl settings

--- static-modules/kvm/manifests/init.pp
+++ static-modules/kvm/manifests/init.pp
@@ -12,4 +12,24 @@ class kvm {
     user        => 'root',
     refreshonly => true
   }
+
+  file_line {'bridge-nf-call-iptables':
+    path => '/etc/sysctl.conf',
+    line => 'net.bridge.bridge-nf-call-iptables = 0'
+  } ->
+
+  file_line {'bridge-nf-call-arptables':
+    path => '/etc/sysctl.conf',
+    line => 'net.bridge.bridge-nf-call-arptables = 0'
+  } ->
+
+  file_line {'bridge-nf-call-ip6tables':
+    path => '/etc/sysctl.conf',
+    line => 'net.bridge.bridge-nf-call-iptables = 0'
+  } ~>
+
+  exec{'/sbin/sysctl -p':
+    user         => 'root',
+    refreshonly => true
+  }
 }
