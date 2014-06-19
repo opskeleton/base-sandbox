f313c130765b7a37e9bc2579a2e13401f84346f2
narkisr <narkisr@gmail.com>
2014-06-20 02:08:09 +0300

    adding cron spec

--- spec/default/clamav_spec.rb
+++ spec/default/clamav_spec.rb
@@ -11,3 +11,7 @@ describe file('/etc/clamav/scans/email_scan') do
    it { should be_mode 500}
    it { should contain 'gookup@gmail.com' }
 end
+
+describe cron do
+    it { should have_entry '18 18 * * * /etc/clamav/scans/all-but-sys-and-dev' }
+end
