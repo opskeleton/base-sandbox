1f3fa51bcc8125115647afb91091aaea666237d1
narkisr <narkisr@gmail.com>
2014-06-21 04:05:29 +0300

    specing synthing conf

--- spec/default/backup_spec.rb
+++ spec/default/backup_spec.rb
@@ -4,3 +4,8 @@ describe file('/etc/duply/s3-ex/conf') do
     it { should contain 'TARGET=s3+http://myUniqueBucketName' }
     it { should contain 'GPG_PW=blabla' }
 end
+
+describe file('/opt/syncthing/.config/syncthing/config.xml') do
+    it { should contain 'tls="true"' }
+    it { should contain '~/appliances-1' }
+end
