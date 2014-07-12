0687e61171de635550e4bb668447e426054e7356
narkisr <narkisr@gmail.com>
2014-07-12 19:29:06 +0300

    latest modules and vagrant spec

---
+++ spec/virtualized/vagrant_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe package('vagrant') do
+    it { should be_installed }
+end
+
+describe command('vagrant version') do
+    it { should return_exit_status 0 }
+end
+
+describe command('pecrant') do
+    it { should return_exit_status 0 }
+end
