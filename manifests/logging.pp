1cbb20ecac0ce28947052a2532537254702e920c
narkisr <narkisr@gmail.com>
2014-07-24 04:11:57 +0300

    ssmpt

--- manifests/logging.pp
+++ manifests/logging.pp
@@ -1,8 +1,16 @@
 node 'logging.local'{
   include apt
-  include shell
-  include vim
-  include baseline
-  include logging
+
+  class{'logging::logwatch':
+    email    => 'foo@gmail.com',
+    mailfrom => 'bar@gmail.com'
+  }
+
+  class{'ssmtp':
+    email   => 'foo@gmail.com',
+    mailhub => 'gmail.com',
+    user    => 'foo',
+    pass    => 'bar'
+  }
 }
 
