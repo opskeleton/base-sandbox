a98f27d9ac7980514fd874d2f3a1a8f52266a3a7
ronen <narkisr@gmail.com>
2014-07-26 00:38:14 +0300

    more specs

--- spec/langs/langs_spec.rb
+++ spec/langs/langs_spec.rb
@@ -7,3 +7,15 @@ end
 describe file('/usr/share/elixir-v0.14.0') do
   it { should be_directory}
 end
+
+describe package('nodejs') do
+  it { should be_installed }
+end
+
+describe command('nodejs') do
+  it { should return_stdout 'v0.10.29' }
+end
+
+describe ppa('launchpad-username/ppa-name') do
+    it { should exist }
+end
