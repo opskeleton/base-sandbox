ee3b0738371756f83f2c9b0e337a5644863c4d66
narkisr <narkisr@gmail.com>
2014-08-18 02:55:11 +0300

    adding self-build

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject puppet-base-env "0.1.0"
+  :description "self-building puppet profiles"
+  :url "https://github.com/narkisr/puppet-base-env"
+  :license  {:name "Apache License, Version 2.0" :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
+
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+
+  :plugins  [[self-build "0.0.6"]]
+
+)
