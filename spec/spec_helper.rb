c2e203d2a3e7dd6f9003b6eace2ae461d71f218f
narkisr <narkisr@gmail.com>
2014-09-20 18:25:03 +0300

    cleaning up and indenting

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,27 +10,26 @@ RSpec.configure do |c|
     c.host  = ENV['TARGET_HOST']
     c.ssh.close if c.ssh
     options = Net::SSH::Config.for(c.host)
-    user    = 'vagrant'
     if(!ENV['LOCAL'])
-	vagrant_up = `vagrant up #{c.host}`
-	config = `vagrant ssh-config #{c.host}`
-	sshhost =  sshuser = ''
-	if config != ''
-	  config.each_line do |line|
-	    if match = /HostName (.*)/.match(line)
-		sshhost = match[1]
-	    elsif  match = /User (.*)/.match(line)
-		sshuser = match[1]
-	    elsif match = /IdentityFile (.*)/.match(line)
-		options[:keys] =  [match[1].gsub(/"/,'')]
-	    elsif match = /Port (.*)/.match(line)
-		options[:port] = match[1]
-	    end
-	  end
-	end
+      `vagrant up #{c.host}`
+      config = `vagrant ssh-config #{c.host}`
+      sshhost =  sshuser = ''
+      if config != ''
+        config.each_line do |line|
+          if match = /HostName (.*)/.match(line)
+            sshhost = match[1]
+          elsif  match = /User (.*)/.match(line)
+            sshuser = match[1]
+          elsif match = /IdentityFile (.*)/.match(line)
+            options[:keys] =  [match[1].gsub(/"/,'')]
+          elsif match = /Port (.*)/.match(line)
+            options[:port] = match[1]
+          end
+        end
+      end
     else
-     sshhost = 'localhost' 
-     sshuser = 'vagrant' 
+      sshhost = 'localhost' 
+      sshuser = 'vagrant' 
     end
     c.ssh = Net::SSH.start(sshhost, sshuser, options)
   end
