ec343b53d897591f560696becb992b325e167477
narkisr <narkisr@gmail.com>
2014-10-04 03:27:37 +0300

    adding sane host and desktop spec

--- manifests/desktop.pp
+++ manifests/desktop.pp
@@ -1,7 +1,12 @@
 node 'desktop.local'{
+
+  class{'desktop::misc':
+    scanner => '1.2.3.4'
+  }
+
   class{'desktop::x2go':
     server => true,
-    lan    => '192.168.1.0/24'
+    lan    => '10.0.2.2'
   }
 }
 
