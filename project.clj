d1fe98122f4ae1f0f116beacf76dd23219c6aa4a
narkisr <narkisr@gmail.com>
2015-02-17 01:27:46 +0200

    using latest self build with fix email

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject puppet-base-env "0.1.0"
+(defproject puppet-base-env "0.1.1"
   :description "self-building puppet profiles"
   :url "https://github.com/narkisr/puppet-base-env"
   :license  {:name "Apache License, Version 2.0" :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
 
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]]
 
-  :plugins  [[self-build "0.0.7"]]
+  :plugins  [[self-build "0.0.9"]]
 
 )
