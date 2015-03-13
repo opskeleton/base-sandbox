86f89031ef859e31fede828e447300a609531ada
narkisr <narkisr@gmail.com>
2015-03-14 00:37:17 +0200

    latest autossh with port_forward'

--- manifests/autossh.pp
+++ manifests/autossh.pp
@@ -1,11 +1,12 @@
 node default {
   include autossh
   autossh::config {'test':
-    user        => 'vagrant',
-    remote_user => 'vagrant',
-    remote_host => 'localhost',
-    remote_port => '22',
-    ssh_id_file => '/home/vagrant/.ssh/id_rsa',
-    ssh_config  => '-R 1234:localhost:22'
+    user         => 'vagrant',
+    remote_user  => 'vagrant',
+    remote_host  => 'localhost',
+    remote_port  => '22',
+    port_forward => '1234',
+    ssh_id_file  => '/home/vagrant/.ssh/id_rsa',
+    ssh_config   => '/home/vagrant/.ssh/config'
   }
 }
