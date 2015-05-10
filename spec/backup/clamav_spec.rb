e6d168eaf88f5dd7a3c6bf03d08b118dd049010b
ronen <narkisr@gmail.com>
2015-05-10 17:09:29 +0300

    adding prefix

--- spec/backup/clamav_spec.rb
+++ spec/backup/clamav_spec.rb
@@ -13,5 +13,5 @@ describe file('/etc/clamav/scans/email_scan') do
 end
 
 describe cron do
-    it { should have_entry '* * * /etc/clamav/scans/all-but-sys-and-dev' }
+    it { should have_entry '43 11 * * * /etc/clamav/scans/all-but-sys-and-dev' }
 end
