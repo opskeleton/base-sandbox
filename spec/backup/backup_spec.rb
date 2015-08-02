624f57c3d52f97a0a00b3bf46deb1ed68d1ca573
ronen <narkisr@gmail.com>
2015-08-03 02:50:33 +0300

    fixing spec

--- spec/backup/backup_spec.rb
+++ spec/backup/backup_spec.rb
@@ -5,9 +5,9 @@ describe file('/etc/duply/s3-ex/conf') do
     it { should contain 'GPG_PW=blabla' }
 end
 
-describe command('zbackup') do
-  its(:stdout) { should include 'Usage' }
-  its(:exit_status) { should eq 0 }
+describe command('/usr/local/bin/zbackup') do
+  its(:stderr) { should include 'Usage' }
+  its(:exit_status) { should eq 1 }
 end
 
 
