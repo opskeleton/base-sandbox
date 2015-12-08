349fdd8b6bdbbc33bb368de1403f793b3973a0b5
narkisr <narkisr@gmail.com>
2015-12-08 15:52:20 +0200

    adding common python package and changing to default

--- manifests/build.pp
+++ manifests/build.pp
@@ -1,8 +1,14 @@
-
-node 'build.local' {
+node default {
   include build
   include barbecue
+
   class{'jdk':
     version => 7
   }
+
+  package{'software-properties-common':
+    ensure  => present
+  } -> Exec <||>
+
+
 }
