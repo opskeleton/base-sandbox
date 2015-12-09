facdb6242dcf364f65027e2102f3755248cc9d78
ronen <narkisr@gmail.com>
2015-12-09 02:30:15 +0200

    latest node

--- spec/langs/langs_spec.rb
+++ spec/langs/langs_spec.rb
@@ -13,7 +13,7 @@ describe package('nodejs') do
 end
 
 describe command('node -v') do
-  its(:stdout) {should contain 'v0.10.40'}
+  its(:stdout) {should contain 'v0.10.41'}
 end
 
 describe command('go version') do
