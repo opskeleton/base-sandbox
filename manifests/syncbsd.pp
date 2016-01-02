bed13afa8ed8614beeeb6dd89e7d456913b19ce9
ronen <narkisr@gmail.com>
2016-01-03 00:19:09 +0200

    removing syncthing

--- manifests/syncbsd.pp
+++ manifests/syncbsd.pp
@@ -1,4 +1,4 @@
-node 'syncbsd.local' {
+node default {
 
   include shell
   include baseline::celestial
@@ -6,25 +6,4 @@ node 'syncbsd.local' {
   class{'vim':
     repo => 'git://github.com/narkisr/vim-lite.git'
   }
-
-  $nodes = {
-    'C56YYFN-U7QEMMU-2J3DVM4-RFHHNAT-FH7ATN6-VJSREZY-XKYXPOF-RSKC7QE' => {name => 'foo' , address => 'foo:1234'},
-  }
-
-
-  $repos = {
-    appliances  => {
-      directory => '/home/vagrant/Syncthing',
-      ro        => false,
-      nodes     => [
-        'C56YYFN-U7QEMMU-2J3DVM4-RFHHNAT-FH7ATN6-VJSREZY-XKYXPOF-RSKC7QE',
-      ]
-    }
-  }
-
-  class{'backup::syncthing':
-    repos => $repos,
-    nodes => $nodes
-  }
-
 }
