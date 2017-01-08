aa2c6ba7214ae128da97d6fb7aac541e9f267113
ronen <narkisr@gmail.com>
2017-01-08 21:35:40 +1100

    increasing timeout

--- manifests/docker.pp
+++ manifests/docker.pp
@@ -1,7 +1,9 @@
 node default {
   include docker
   include shell::fig
-  docker::image {'library/redis':}
+  docker::image {'library/redis':
+    timeout => 360
+  }
   docker::service{'library/redis':}
 
 }
