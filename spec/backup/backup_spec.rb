904aff8f5e91eb95353800f82c345df70302845c
ronen <narkisr@gmail.com>
2017-03-10 03:00:05 +1100

    fixing spec

--- spec/backup/backup_spec.rb
+++ spec/backup/backup_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe command('/usr/local/bin/zbackup') do
+describe command('/usr/bin/zbackup') do
   its(:stderr) { should include 'Usage' }
   its(:exit_status) { should eq 1 }
 end
