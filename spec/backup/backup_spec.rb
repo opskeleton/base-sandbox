d33e51c138e14f3f6636a11a2069ecc0a8ccd989
ronen <narkisr@gmail.com>
2017-03-09 22:02:34 +1100

    removing duply

--- spec/backup/backup_spec.rb
+++ spec/backup/backup_spec.rb
@@ -1,10 +1,5 @@
 require 'spec_helper'
 
-describe file('/etc/duply/s3-ex/conf') do
-    it { should contain 'TARGET=s3+http://myUniqueBucketName' }
-    it { should contain 'GPG_PW=blabla' }
-end
-
 describe command('/usr/local/bin/zbackup') do
   its(:stderr) { should include 'Usage' }
   its(:exit_status) { should eq 1 }
