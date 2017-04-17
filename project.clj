3b01d086a38be74002fb82131f276169489f30a5
ronen <narkisr@gmail.com>
2017-04-17 18:29:40 +1000

    renaming leftovers

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject puppet-base-env "0.1.1"
+(defproject base-sandbox "0.4.0"
   :description "self-building puppet profiles"
-  :url "https://github.com/narkisr/puppet-base-env"
+  :url "https://github.com/opskeleton/base-sandbox"
   :license  {:name "Apache License, Version 2.0" :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
 
   :dependencies [[org.clojure/clojure "1.6.0"]]
