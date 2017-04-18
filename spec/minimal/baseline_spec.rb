007adbc6a7925f66ecca17659743ad0f953fc0e2
ronen <narkisr@gmail.com>
2017-04-19 01:25:12 +1000

    removing celestial

--- spec/minimal/baseline_spec.rb
+++ spec/minimal/baseline_spec.rb
@@ -23,16 +23,6 @@ describe file('/etc/sudoers.d/upgrade') do
     it { should be_file }
 end
 
-
-describe user('celestial') do
-  it { should exist }
-end
-
-describe file('/etc/sudoers.d/celestial') do
-    it { should be_file }
-    it { should contain 'celestial ALL=NOPASSWD: /tmp/*/scripts/run.sh  --detailed-exitcodes'}
-end
-
 describe package('rng-tools') do
     it { should be_installed }
 end
