87b637dd183f160ad75b2efdf62db9445683a7b3
ronen <narkisr@gmail.com>
2017-06-18 00:27:01 +1000

    using includes and adding dropbox (commented out)

--- manifests/backup.pp
+++ manifests/backup.pp
@@ -1,7 +1,14 @@
 node default {
   include clamav
+  include backup::zbackup
+  include backup::rclone
+  include backup::octo
   include apt
 
+  # class {'backup::dropbox':
+  #  headless => true
+  # }
+  #
   clamav::scan { 'all-but-sys-and-dev':
     exclude_dir => ['(?i)/^\/(sys|dev)/'],
     scan        => ['/'],
@@ -9,15 +16,9 @@ node default {
     email       => 'gookup@gmail.com'
   }
 
-
-  class{'backup::zbackup':}
-  class{'backup::rclone':}
-  class{'backup::octo':}
-
   package{'software-properties-common':
     ensure  => present
   } -> Exec <||>
-
 }
 
 
