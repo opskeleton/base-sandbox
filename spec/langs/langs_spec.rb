01a1265785ac82463892a2bbd6ddc807ffd7b2cd
ronen <narkisr@gmail.com>
2017-07-15 23:19:53 +1000

    version up

--- spec/langs/langs_spec.rb
+++ spec/langs/langs_spec.rb
@@ -13,7 +13,7 @@ describe package('nodejs') do
 end
 
 describe command('node -v') do
-  its(:stdout) {should contain 'v4.8.3'}
+  its(:stdout) {should contain 'v4.8.4'}
 end
 
 describe command('go version') do
