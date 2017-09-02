057ae74b672482251293bfb74faa1c170030c814
ronen <narkisr@gmail.com>
2017-09-03 02:01:15 +1000

    fixing run

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 MANIFEST=manifests/default.pp
 ENV=prod
-puppet apply --modulepath=modules:static-modules ${1:-$MANIFEST} --hiera_config hiera.yaml --environment=${2:-$ENV} "${@:3}"
+puppet apply --modulepath=modules:static-modules $MANIFEST --hiera_config hiera.yaml --environment=$ENV $@
