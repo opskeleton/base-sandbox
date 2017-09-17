f7bae48f9e10bcce4a2b74e917b5966357cae182
ronen <narkisr@gmail.com>
2017-09-18 01:25:21 +1000

    mirror

--- manifests/minimal.pp
+++ manifests/minimal.pp
@@ -1,9 +1,9 @@
 node default {
-  include apt
   include shell
   include vim
   include git::configure
   include baseline
+  include baseline::mirror
   include baseline::ssh::config
   include baseline::update
   include timezone
