99fc0d821302645ce2a049087e7d391ee4af298c
ronen <narkisr@gmail.com>
2018-05-12 00:31:09 +1000

    backup works

--- spec/backup/backup_spec.rb
+++ spec/backup/backup_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe command('/usr/bin/zbackup') do
-  its(:stderr) { should include 'Usage' }
+  its(:stdout) { should include 'Usage' }
   its(:exit_status) { should eq 1 }
 end
 
