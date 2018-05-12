bddf5959eb7a9efb92ce37e92573d90476b2b683
ronen <narkisr@gmail.com>
2018-05-13 01:56:42 +1000

    more specs

--- spec/backup/backup_spec.rb
+++ spec/backup/backup_spec.rb
@@ -5,4 +5,14 @@ describe command('/usr/bin/zbackup') do
   its(:exit_status) { should eq 1 }
 end
 
+describe command('/usr/bin/octo') do
+  its(:stdout) { should include '0.8.0' }
+end
 
+describe command('/usr/bin/restic version') do
+  its(:stdout) { should include '0.8.3' }
+end
+
+describe command('/usr/bin/rclone version') do
+  its(:stdout) { should include '1.36' }
+end
